05c382c76d916b6893b71efd81bc12df222a57dc
SyedMughisHussain <syedmughis4541@gmail.com>
2025-02-21 12:22:35 +0500

    Custom input component created and implemented successfully

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "expo-router";
 import { Button, Text } from "react-native";
+import CustomInput from "@/components/CustomInput";
 
 export default function Login() {
   const navigation = useRouter();
@@ -13,6 +14,11 @@ export default function Login() {
     <>
       <Text>This is an login page</Text>
       <Button title="Push" onPress={hnadleNavigation} />
+      <CustomInput
+        placeholder="3193039832"
+        maxLength={50}
+        onChangeText={hnadleNavigation}
+      />
     </>
   );
 }
---
+++ components/CustomInput.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { TextInput, StyleSheet, View, Text, Image } from "react-native";
+
+type CustomInputProps = {
+  placeholder: string;
+  onChangeText: (string: any) => void;
+  maxLength?: number;
+};
+
+export default function CustomInput({
+  placeholder,
+  onChangeText,
+  maxLength,
+}: CustomInputProps) {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingLeft: 8,
+          paddingRight: 8,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            color: "#B0ABAB",
+            fontWeight: 500,
+            paddingHorizontal: 8,
+            paddingVertical: 2,
+            lineHeight: 24,
+          }}
+        >
+          +92
+        </Text>
+        <Image source={require("../assets/images/subtract.png")} />
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        onChange={onChangeText}
+        keyboardType="number-pad"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 10,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#B0ABAB",
+    paddingTop: 6,
+    paddingRight: 16,
+    paddingLeft: 16,
+    paddingBottom: 6,
+    borderRadius: 8,
+    fontSize: 18,
+    fontWeight: 500,
+    width: 263,
+    height: 48,
+  },
+});
